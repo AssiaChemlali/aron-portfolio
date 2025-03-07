c73f2a5912da951579d20b8e70da0d462df5336b
AssiaChemlali <chemlali.assia@gmail.com>
2025-03-07 10:51:32 +0100

    add navbar

---
+++ src/components/ShinyEffect.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ShinyEffect = ({left,right,top,size=500}) => {
+
+  const positi
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShinyEffect
